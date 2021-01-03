228ceb1347526025e1add216ab7a1e4d8b87c2c2
Car-png <cardenasjavier1846@gmail.com>
2021-01-02 20:23:00 -0500

    JCardenas

--- Js/Main.js
+++ Js/Main.js
@@ -5,11 +5,33 @@ $(document).ready(function () {
 		}else{
 			$('.navbar').removeClass("sticky");
 		}
-	})
+	});
 
 	// toggle menu/navbar script
 	$('.menu-btn').click(function () { 
 		$('.navbar .menu').toggleClass("active");
 		$('.menu-btn i').toggleClass("active");
+	});
+
+	// owl carousel script
+	$('.carousel').owlCarousel({
+		margin: 20,
+		loop: true,
+		autoplayTimeOut: 2000,
+		autouplayHoverPause: true,
+		responsive:{
+			0:{
+				items: 1,
+				nav: true
+			},
+			600:{
+				items: 2,
+				nav: false
+			},
+			1000:{
+				items: 3,
+				nav: false
+			}
+		}
 	});
 });
