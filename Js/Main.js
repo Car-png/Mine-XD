558952e64f54620a3f245460d188e7d6cb13b3bb
Car-png <cardenasjavier1846@gmail.com>
2021-01-05 17:08:37 -0500

    JCardenas

--- Js/Main.js
+++ Js/Main.js
@@ -2,18 +2,50 @@ $(document).ready(function () {
 	$(window).scroll(function () {
 		if(this.scrollY > 20){
 			$('.navbar').addClass("sticky");
-		}else{
+		}
+		else{
 			$('.navbar').removeClass("sticky");
+		}
+
+		if(this.scrollY > 500){
+			$('.scroll-up-btn').addClass("show");
+		}
+		else{
+			$('.scroll-up-btn').removeClass("show");
 		}
 	});
 
-	// toggle menu/navbar script
+	// ! slide-up script
+	$('.scroll-up-btn').click(function(){
+		$('html').animate({scrollTop: 0});
+	});
+
+
+	// ! Typing animation scrip
+	var typed = new Typed(".typing", {
+		strings: ['Development', 'Designer', 'Student'],
+		typeSpeed: 100,
+		backSpeed: 60,
+		loop: true
+	});
+
+	var typed = new Typed(".typing-2", {
+		strings: ['Development', 'Designer', 'Student'],
+		typeSpeed: 100,
+		backSpeed: 60,
+		loop: true
+	});
+
+
+	// ! toggle menu/navbar script
 	$('.menu-btn').click(function () { 
 		$('.navbar .menu').toggleClass("active");
 		$('.menu-btn i').toggleClass("active");
 	});
 
-	// owl carousel script
+
+
+	// ! Owl carousel script
 	$('.carousel').owlCarousel({
 		margin: 20,
 		loop: true,
